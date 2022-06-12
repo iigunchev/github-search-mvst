d69413b50575ff887dd938589d4e5982556b240f
iigunchev <iigunchev@gmail.com>
2022-06-12 11:16:48 +0200

    header stories

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 
-const Header = () => {
+const Header = ({ title }: { title: string }) => {
   return (
     <header className="bg-dark p-4">
       <Link href="/">
-        <span className="text-slate-50 cursor-pointer text-lg">GitSearch</span>
+        <span className="text-slate-50 cursor-pointer text-lg">{title}</span>
       </Link>
     </header>
   );
